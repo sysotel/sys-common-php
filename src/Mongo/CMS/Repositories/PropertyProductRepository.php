9862a4a8a279d9a88fe646b63c3f100dd7ddb59e
Ravish <cto@sysotel.com>
2022-07-24 22:34:24 +0530

    update

--- src/Mongo/CMS/Repositories/PropertyProductRepository.php
+++ src/Mongo/CMS/Repositories/PropertyProductRepository.php
@@ -2,7 +2,7 @@
 
 namespace SYSOTEL\APP\Common\Mongo\CMS\Repositories;
 
-use Delta4op\Mongodb\DocumentRepositories\DocumentRepository;
+use Delta4op\Mongodb\Repositories\DocumentRepository;
 
 class PropertyProductRepository extends DocumentRepository
 {
