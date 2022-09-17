33e3d29eceff120ce6c070c8f6464192923abaac
Ravish <cto@sysotel.com>
2022-09-17 12:16:36 +0530

    domain rule added

---
+++ src/Rules/Domain.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace SYSOTEL\APP\Common\Rules;
+
+class Domain extends RegexRule
+{
+    /**
+     * Regex pattern
+     *
+     * @return string
+     */
+    public function pattern(): string
+    {
+        return '/^([a-zA-Z0-9][a-zA-Z0-9-_]*\.)*[a-zA-Z0-9]*[a-zA-Z0-9-_]*[[a-zA-Z0-9]+$/';
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message(): string
+    {
+        return 'Invalid PAN number';
+    }
+}
