60813de49a01258b3880eddb68cb673767958a13
Ravish <cto@sysotel.com>
2022-09-26 20:09:11 +0530

    update

--- src/Mongo/CMS/Documents/PropertyImage/ImageItem.php
+++ src/Mongo/CMS/Documents/PropertyImage/ImageItem.php
@@ -4,7 +4,7 @@ namespace SYSOTEL\APP\Common\Mongo\CMS\Documents\PropertyImage;
 
 use Delta4op\Mongodb\Documents\EmbeddedDocument;
 use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
-use SYSOTEL\APP\Common\Enums\CMS\PropertyImageVersion;
+use SYSOTEL\APP\Common\Enums\CMS\PropertyImageTarget;
 
 /**
  * @ODM\EmbeddedDocument
@@ -12,8 +12,8 @@ use SYSOTEL\APP\Common\Enums\CMS\PropertyImageVersion;
 class ImageItem extends EmbeddedDocument
 {
     /**
-     * @var PropertyImageVersion
-     * @ODM\Field(type="string", enumType=SYSOTEL\APP\Common\Enums\CMS\PropertyImageVersion::class)
+     * @var PropertyImageTarget
+     * @ODM\Field(type="string", enumType=SYSOTEL\APP\Common\Enums\CMS\PropertyImageTarget::class)
      */
     public $target;
 
@@ -22,4 +22,10 @@ class ImageItem extends EmbeddedDocument
      * @ODM\Field(type="string")
      */
     public $filePath;
+
+    /**
+     * @var ImageMetadata
+     * @ODM\EmbedOne(targetDocument=ImageMetadata::class)
+     */
+    public $meta;
 }
