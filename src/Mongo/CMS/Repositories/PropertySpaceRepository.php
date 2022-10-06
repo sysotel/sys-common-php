8de367ca8dcd05661d73c244af163e1f912bb6b8
ravish <ravish@sysotel.ai>
2022-10-06 12:40:25 +0530

    update

--- src/Mongo/CMS/Repositories/PropertySpaceRepository.php
+++ src/Mongo/CMS/Repositories/PropertySpaceRepository.php
@@ -3,6 +3,7 @@
 namespace SYSOTEL\APP\Common\Mongo\CMS\Repositories;
 
 use Delta4op\Mongodb\Repositories\DocumentRepository;
+use Doctrine\ODM\MongoDB\LockMode;
 use SYSOTEL\APP\Common\Enums\CMS\InventoryAccuracy;
 use SYSOTEL\APP\Common\Enums\CMS\PropertySpaceStatus;
 use SYSOTEL\APP\Common\Mongo\CMS\Documents\Property\Property;
@@ -10,6 +11,19 @@ use SYSOTEL\APP\Common\Mongo\CMS\Documents\PropertySpace\PropertySpace;
 
 class PropertySpaceRepository extends DocumentRepository
 {
+    /**
+     * @param int $spaceId
+     * @param Property|int $propertyId
+     * @return PropertySpace|null
+     */
+    public function findSpaceForProperty(int $spaceId, Property|int $propertyId): PropertySpace|null
+    {
+        return $this->findOneBy([
+            '_id' => $spaceId,
+            'propertyId' => Property::resolveID($propertyId)
+        ]);
+    }
+
     /**
      * @param int $spaceId
      * @param Property|int $property
