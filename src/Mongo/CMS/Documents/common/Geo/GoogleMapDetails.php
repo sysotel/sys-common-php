afe462d2c6984530797ecaa51719fbef51a94b00
ravish <ravish@sysotel.ai>
2023-03-13 11:02:00 +0530

    update

--- src/Mongo/CMS/Documents/common/Geo/GoogleMapDetails.php
+++ src/Mongo/CMS/Documents/common/Geo/GoogleMapDetails.php
@@ -12,48 +12,48 @@ use Delta4op\Mongodb\Documents\EmbeddedDocument;
 class GoogleMapDetails extends EmbeddedDocument
 {
     /**
-     * @var string
+     * @var ?string
      * @ODM\Field(type="string")
      */
     protected $placeId;
 
     /**
-     * @var string
+     * @var ?string
      * @ODM\Field(type="string")
      */
     protected $phone;
 
     /**
-     * @return string
+     * @return string|null
      */
-    public function getPlaceId(): string
+    public function getPlaceId(): ?string
     {
         return $this->placeId;
     }
 
     /**
-     * @param string $placeId
+     * @param string|null $placeId
      * @return GoogleMapDetails
      */
-    public function setPlaceId(string $placeId): GoogleMapDetails
+    public function setPlaceId(?string $placeId): GoogleMapDetails
     {
         $this->placeId = $placeId;
         return $this;
     }
 
     /**
-     * @return string
+     * @return string|null
      */
-    public function getPhone(): string
+    public function getPhone(): ?string
     {
         return $this->phone;
     }
 
     /**
-     * @param string $phone
+     * @param string|null $phone
      * @return GoogleMapDetails
      */
-    public function setPhone(string $phone): GoogleMapDetails
+    public function setPhone(?string $phone): GoogleMapDetails
     {
         $this->phone = $phone;
         return $this;
