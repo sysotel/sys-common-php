d6c6804df04eac889130f044e3fa4cf840b7125d
Nikki <nikkig@sysotel.ai>
2023-03-14 14:27:26 +0530

    changes

--- src/Mongo/CMS/Documents/common/Geo/GoogleMapDetails.php
+++ src/Mongo/CMS/Documents/common/Geo/GoogleMapDetails.php
@@ -18,6 +18,54 @@ class GoogleMapDetails extends EmbeddedDocument
     protected $placeId;
 
     /**
+     * @var string
+     * @ODM\Field(type="string")
+     */
+    protected $name;
+
+    /**
+     * @var string
+     * @ODM\Field(type="string")
+     */
+    protected $addr1;
+
+    /**
+     * @var string
+     * @ODM\Field(type="string")
+     */
+    protected $city;
+
+    /**
+     * @var string
+     * @ODM\Field(type="string")
+     */
+    protected $province;
+
+    /**
+     * @var string
+     * @ODM\Field(type="string")
+     */
+    protected $country;
+
+    /**
+     * @var string
+     * @ODM\Field(type="string")
+     */
+    protected $postalCode;
+
+    /**
+     * @var string
+     * @ODM\Field(type="string")
+     */
+    protected $longitude;
+
+    /**
+     * @var string
+     * @ODM\Field(type="string")
+     */
+    protected $latitude;
+
+    /**
      * @return string
      */
     public function getPlaceId(): string
@@ -35,4 +83,131 @@ class GoogleMapDetails extends EmbeddedDocument
         return $this;
     }
 
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAddr1(): string
+    {
+        return $this->addr1;
+    }
+
+    /**
+     * @param string $addr1
+     */
+    public function setAddr1(string $addr1): void
+    {
+        $this->addr1 = $addr1;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCity(): string
+    {
+        return $this->city;
+    }
+
+    /**
+     * @param string $city
+     */
+    public function setCity(string $city): void
+    {
+        $this->city = $city;
+    }
+
+    /**
+     * @return string
+     */
+    public function getProvince(): string
+    {
+        return $this->province;
+    }
+
+    /**
+     * @param string $province
+     */
+    public function setProvince(string $province): void
+    {
+        $this->province = $province;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCountry(): string
+    {
+        return $this->country;
+    }
+
+    /**
+     * @param string $country
+     */
+    public function setCountry(string $country): void
+    {
+        $this->country = $country;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPostalCode(): string
+    {
+        return $this->postalCode;
+    }
+
+    /**
+     * @param string $postalCode
+     */
+    public function setPostalCode(string $postalCode): void
+    {
+        $this->postalCode = $postalCode;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLongitude(): string
+    {
+        return $this->longitude;
+    }
+
+    /**
+     * @param string $longitude
+     */
+    public function setLongitude(string $longitude): void
+    {
+        $this->longitude = $longitude;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLatitude(): string
+    {
+        return $this->latitude;
+    }
+
+    /**
+     * @param string $latitude
+     */
+    public function setLatitude(string $latitude): void
+    {
+        $this->latitude = $latitude;
+    }
 }
