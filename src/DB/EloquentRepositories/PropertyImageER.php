103021cea6fc3dc608d3144b53bb0ef18e3473e8
Nikki <nikkig@sysotel.ai>
2023-04-29 13:46:07 +0530

    changes

--- src/DB/EloquentRepositories/PropertyImageER.php
+++ src/DB/EloquentRepositories/PropertyImageER.php
@@ -2,19 +2,12 @@
 
 namespace SYSOTEL\APP\Common\DB\EloquentRepositories;
 
-use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Model;
-use SYSOTEL\APP\Common\DB\EloquentQueryBuilders\PropertyProductEQB;
-use SYSOTEL\APP\Common\DB\EloquentQueryBuilders\PropertySpaceEQB;
+
 use SYSOTEL\APP\Common\DB\Models\Property\Property;
 use SYSOTEL\APP\Common\DB\Models\PropertyImage\PropertyImage;
-use SYSOTEL\APP\Common\DB\Models\PropertyProduct\PropertyProduct;
 use SYSOTEL\APP\Common\DB\Models\PropertySpace\PropertySpace;
-use SYSOTEL\APP\Common\Enums\CMS\Account;
 use SYSOTEL\APP\Common\Enums\CMS\PropertyImageStatus;
 use SYSOTEL\APP\Common\Enums\CMS\PropertyImageTarget;
-use SYSOTEL\APP\Common\Enums\CMS\PropertyProductStatus;
-use SYSOTEL\APP\Common\Enums\CMS\PropertySpaceStatus;
 
 class PropertyImageER extends EloquentRepository
 {
