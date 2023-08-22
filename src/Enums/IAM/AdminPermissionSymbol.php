73981a0c9cddcb22547a8a120435fc43981d6455
Nikki <nikkig@sysotel.ai>
2023-08-22 11:55:52 +0530

    chnages

--- src/Enums/IAM/AdminPermissionSymbol.php
+++ src/Enums/IAM/AdminPermissionSymbol.php
@@ -304,6 +304,10 @@ enum AdminPermissionSymbol: string
 
     case IBE_ApiLogs_DownloadRequestData = 'IBE:ApiLogs:DownloadRequestData';
 
+    case IBE_ApiLogs_DownloadResponseData = 'IBE_ApiLogs_DownloadResponseData';
+
+
+
 
 // Website Settings
 
